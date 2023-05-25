76e497ddb810a859aae0be85392460f124d2c19c
Artemius <cpt.morgan0510@gmail.com>
2023-05-25 17:03:54 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public int calculate(int a, double b) { // a - кг, b - м
+    public int calculate(int weight, double height) { // Система СИ: Weight(Вес) - в кг, height(рост) - в м
         double result;
-        result = a / Math.pow(b, 2);
+        result = weight / Math.pow(height, 2);
         return (int) result;
     }
 }
